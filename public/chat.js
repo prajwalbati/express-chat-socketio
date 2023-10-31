6526c24010fb14429a176f378e11b775e2d28c4f
Prajwal Bati <prajwalbati@gmail.com>
2023-10-31 15:18:18 -0400

    update UI

--- public/chat.js
+++ public/chat.js
@@ -4,8 +4,9 @@ $(function() {
 
     let message = $("#message")
     let username = $("#username")
-    let send_message = $("#send_message")
-    let send_username = $("#send_username")
+    let sendMessageBtn = $("#sendMessageBtn");
+    let sendMessageForm = $("#sendMessageForm");
+    let setUsernameBtn = $("#setUsernameBtn")
     let chatroom = $("#chats")
     let feedback = $("#feedback");
 
@@ -14,44 +15,46 @@ $(function() {
     };
 
     //emit a username
-    send_username.click(function() {
+    setUsernameBtn.click(function() {
         let usernameValue = username.val();
         if (usernameValue) {
             $("#usernameLabel, #chatroom, #input_zone").removeClass("hidden");
             $("#usernameLabel label").text(usernameValue);
-            $("#connectMessage, #change_username").addClass("hidden");
+            $("#change_username").addClass("hidden");
             socket.emit("change_username", {username:usernameValue}, () => {
-                chatroom.append("<p class='systemMessage'>You entered chatroom. Welcome "+usernameValue+"</p>")
+                chatroom.append("<p class='systemMessage text-center italic'>You entered chatroom. Welcome "+usernameValue+"</p>")
                 scrollChat();
             });
         }
     });
 
     socket.on("new_user_enters", (data)=>{
-        chatroom.append("<p class='systemMessage'>"+data.username+" entered chatroom. Welcome "+data.username+"</p>");
+        chatroom.append("<p class='systemMessage text-center italic'>"+data.username+" entered chatroom. Welcome "+data.username+"</p>");
         scrollChat();
     })
 
     // emit message
-    send_message.click(function(e) {
+    sendMessageForm.submit(function(e) {
         e.preventDefault();
-        socket.emit("new_message", { username: username.val(), message: message.val() }, () => {
-            // ack
-        });
+        if (message.val()) {
+            socket.emit("new_message", { username: username.val(), message: message.val() }, () => {
+                // ack
+            });
 
-        let messageBox = `<div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
-            <div>
-                <div class="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
-                    <p class="text-sm">${message.val()}</p>
+            let messageBox = `<div class="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
+                <div>
+                    <div class="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
+                        <p class="text-sm">${message.val()}</p>
+                    </div>
+                    <span class="text-xs text-gray-500 leading-none">2 min ago</span>
                 </div>
-                <span class="text-xs text-gray-500 leading-none">2 min ago</span>
-            </div>
-            <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-        </div>`;
+                <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+            </div>`;
 
-        chatroom.append(messageBox);
-        message.val("");
-        scrollChat();
+            chatroom.append(messageBox);
+            message.val("");
+            scrollChat();
+        }
     })
 
     //listen to message
